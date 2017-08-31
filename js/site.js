23118937851e2aa1a10b54686ed056dc857b7643
mokoaki <mokoriso@gmail.com>
2017-09-01 00:39:54 +0900

    tekitou

--- js/site.js
+++ js/site.js
@@ -6,15 +6,7 @@
       var sayu = event.clientX / $window.width()  * -10 + 5;
       var joge = event.clientY / $window.height() * -10 + 5;
 
-       $("body").css("margin", (joge + "px ") + (-sayu + "px ") + (-joge + "px ") +(sayu + "px"));
+       $("body").css({"margin": joge + "px " + (-sayu) + "px " + (-joge) + "px " + sayu + "px"});
     }, true);
   }, true);
 })();
-
-
-  // $(document).on "mousemove", (event) ->
-  //   sayu = event.clientX / $window.width()  * -10 + 5
-  //   joge = event.clientY / $window.height() * -10 + 5
-  //
-  //   $("html").css
-  //     "margin" : "#{joge}px #{-sayu}px #{-joge}px #{sayu}px"
