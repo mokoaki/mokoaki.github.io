9426b823eeb0012082e0f145c7fd0e9fd6e075c4
mokoaki <mokoriso@gmail.com>
2017-09-01 00:48:37 +0900

    tekitou

--- js/site.js
+++ js/site.js
@@ -2,8 +2,8 @@
   "use strict";
 
   document.addEventListener("mousemove", function(event) {
-    var sayu = event.clientX / $(window).width()  * -6 + 3;
-    var joge = event.clientY / $(window).height() * -6 + 3;
+    var sayu = event.clientX / $(window).width()  * -4 + 2;
+    var joge = event.clientY / $(window).height() * -4 + 2;
 
      $("body").css({"margin": joge + "px " + (-sayu) + "px " + (-joge) + "px " + sayu + "px"});
   }, true);
