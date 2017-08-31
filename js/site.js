d544bc8d02f89e80f07001ef108f21fbb9b6f816
mokoaki <mokoriso@gmail.com>
2017-09-01 00:41:03 +0900

    tekitou

--- js/site.js
+++ js/site.js
@@ -1,12 +1,10 @@
 (function () {
   "use strict";
 
-  document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("mousemove", function(event) {
-      var sayu = event.clientX / $window.width()  * -10 + 5;
-      var joge = event.clientY / $window.height() * -10 + 5;
+  document.addEventListener("mousemove", function(event) {
+    var sayu = event.clientX / $window.width()  * -10 + 5;
+    var joge = event.clientY / $window.height() * -10 + 5;
 
-       $("body").css({"margin": joge + "px " + (-sayu) + "px " + (-joge) + "px " + sayu + "px"});
-    }, true);
+     $("body").css({"margin": joge + "px " + (-sayu) + "px " + (-joge) + "px " + sayu + "px"});
   }, true);
 })();
